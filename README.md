548929b0f8e9738fb56522ece01daf24a6d0134f
Gnlow <gnlow@protonmail.com>
2023-05-21 22:02:28 +0900

    working

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ufcs
+
+```ts
+const Duck = ufcs(class Duck {
+    quack() {
+        console.log("Quack!")
+    }
+})
+Duck.quack(new Duck)
+
+const d = new Duck
+```
--- mod.ts
+++ mod.ts
@@ -1,44 +1,16 @@
-function ufcs(method: any, context: ClassMethodDecoratorContext) {
-    method.ufcs = true
-}
-
-function ufcs2(_class: any, context: ClassDecoratorContext) {
-    console.log(context)
-    Object.getOwnPropertyNames(_class.prototype)
-        .forEach(prop => {
-            console.log(prop)
-            if (_class.prototype[prop].ufcs) {
-                _class[prop] = (_this: any) => _this[prop]()
-            }
-        })
-    return _class
-}
-type B<C extends {"prototype": any}> = {
+type Ufcs<C extends {"prototype": any}> = {
     [K in keyof C["prototype"]]:
         (_this: C) => C["prototype"][K]
 } & { new (): C }
 
-type KK = B<typeof Duck> & typeof Duck
-
-const aa = {a: 1}
-
-function ufcsr(_$: any, context: any) {
-    console.log("CCCC", context)
-    console.log("CCCCCC", _$)
+const ufcs = <C extends {"prototype": any}>(_class: C) => {
+    Object.getOwnPropertyNames(_class.prototype)
+        .forEach(prop => {
+            if (prop != "prototype") {
+                Object.defineProperty(_class, prop, {
+                    value: (_this: any) => _this[prop]()
+                })
+            }
+        })
+    return _class as unknown as Ufcs<C>
 }
-
-@ufcs2
-class _Duck {
-    @ufcs
-    quack() {
-        console.log("Quack!")
-    }
-    static t() {}
-    a(){}
-}
-
-const Duck = _Duck as unknown as B<typeof _Duck>
-
-Duck.quack(new Duck)
-
-const d = new Duck
